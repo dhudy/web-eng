7acfc9ae7f4e98be21f1019f46099adcb49e8959
Daniel Hudy <djh3315@rit.edu>
2015-02-27 15:11:01 -0500

    Dashing configrations

--- config/initializers/dashing.rb
+++ config/initializers/dashing.rb
@@ -5,8 +5,8 @@ Dashing.configure do |config|
 
   # Redis credentials.
   # See https://devcenter.heroku.com/articles/redistogo to configure redis for heroku.
-  # config.redis_host     = '127.0.0.1'
-  # config.redis_port     = '6379'
+  config.redis_host     = 'redis://redistogo:5f07a04d1364078ac6c0c6ed16d4f3c0@gar.redistogo.com'
+  config.redis_port     = '9382'
   # config.redis_password = nil
   # config.redis_timeout  = 3
 
